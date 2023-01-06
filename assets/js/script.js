aef0627c6db3d09db09e82740d07d3528d0b9ca6
misonwill <57513777+misonwill@users.noreply.github.com>
2023-01-06 23:18:45 +0800

    initial changes

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+function menuToggle() {
+    var toggle = document.getElementById("menu_toggle");
+    var menu = document.getElementById("mobile_menu");
+    if (toggle.checked == true){
+        menu.style.display = "block";
+    } else {
+        menu.style.display = "none";
+    }
+}
