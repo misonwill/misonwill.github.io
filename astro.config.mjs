3f1332ac05fd2c8489c6e4b8c50cf06d1f8f22b1
misonwill <57513777+misonwill@users.noreply.github.com>
2023-02-06 13:54:50 +0800

    archive updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://misonwill.github.io/'
+    site: 'https://misonwill.github.io/',
+    markdown: {
+        syntaxHighlight: 'prism',
+      },
 });
