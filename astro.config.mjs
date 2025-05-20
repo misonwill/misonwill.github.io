23c89d6ee55ff4c7eeb707634b1626724f78ade8
whiskliz <jimmyeagle57@gmail.com>
2025-05-20 15:39:33 +0800

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://williammison.online/',
+    site: 'https://misonwill.github.io/',
     markdown: {
         syntaxHighlight: 'prism',
       },
